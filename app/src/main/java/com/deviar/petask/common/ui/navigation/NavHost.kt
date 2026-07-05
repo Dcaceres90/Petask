20ee396245c66aaf40be5db1ca2351f612b24c6c
IaraOar <iara.empresita@gmail.com>
2026-07-05 14:14:46 -0300

    Create petScreen UI

--- app/src/main/java/com/deviar/petask/common/ui/navigation/NavHost.kt
+++ app/src/main/java/com/deviar/petask/common/ui/navigation/NavHost.kt
@@ -1,7 +1,6 @@
 package com.deviar.petask.common.ui.navigation
 
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
